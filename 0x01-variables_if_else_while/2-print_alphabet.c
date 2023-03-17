9958f8c4d11877ec87c5b4adc349c1569a377b83
Nmcleon <nmcleon10@gmail.com>
2023-03-17 08:26:24 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point of the program
+*
+* Return: 0 on success
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}

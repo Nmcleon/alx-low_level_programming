fa2756aa2028522080e0e0affab3d72af71385a5
Nmcleon <nmcleon10@gmail.com>
2023-03-21 17:05:23 -0700

    again

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0.
- */
-
+*times_table - prints the 9 times table, starting with 0
+*/
 void times_table(void)
 {
-    int i, j, result;
+int i, j, k;
 
-    _putchar('0');
-    _putchar('\n'); // Print the first line (0 times 9)
-    
-    for (i = 1; i <= 9; i++) {
-        for (j = 0; j <= 9; j++) {
-            result = i * j;
-            if (result <= 9) {
-                if (j > 0) {
-                    _putchar(' ');
-                }
-                _putchar(result + '0');
-            } else {
-                _putchar(result / 10 + '0');
-                _putchar(result % 10 + '0');
-            }
-            if (j < 9) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n'); // Print a newline after each row
-    }
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
 }
+if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+

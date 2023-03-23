6291324a8fa387a37bbe1591aad2bbdd28fef71e
N <51336368+Nmcleon@users.noreply.github.com>
2023-03-23 10:48:00 +0200

    first 98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 /**
- *main - print first 98 fibonacci
- *
- *Return: 0 always.
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-	int first = 1, second = 2, next;
-	int count = 2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%d, %d", first, second);
+	j = 1;
+	k = 2;
 
-	while (count < 98)
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = first + second;
-		printf(", %d", next);
-		first = second;
-		second = next;
-		count++;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
 
-	next = first + second;
-	printf("%d\n", next);
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
 
 	return (0);
 }

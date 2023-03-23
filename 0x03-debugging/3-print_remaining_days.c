ad3452e9e5b6ced2a7084bb10b2359747fba1898
Nmcleon <nmcleon10@gmail.com>
2023-03-23 09:15:19 -0700

    3-main_b.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,34 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
+	int days_in_feb = 28;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		days_in_feb = 29;
+	}
+
+	if (month < 1 || month > 12 || day < 1 || day > 31)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+	}
+	else if (month == 2 && day > days_in_feb)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		int day_of_year = convert_day(month, day);
+		int remaining_days = 365;
+
+		if (days_in_feb == 29)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			remaining_days = 366;
 		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+
+		remaining_days -= day_of_year;
+
+		printf("Day of the year: %d\n", day_of_year);
+		printf("Remaining days: %d\n", remaining_days);
 	}
 }

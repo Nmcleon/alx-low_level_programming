0aed3e69adb04a82d75c25333cbecc9d3ed6b587
N <51336368+Nmcleon@users.noreply.github.com>
2023-03-27 20:35:52 +0200

    prints half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,19 @@
  */
 void puts_half(char *str)
 {
-	int len = 0, i, n;
+int i, last;
 
-	while (str[len] != '\0')
-		len++;
-
-	if (len % 2 == 0)
-		n = len / 2;
-	else
-		 n = (len - 1) / 2;
-
-	for (i = n; i < len; i++)
-		_putchar(str[i]);
-
-	_putchar('\n');
+i = 0;
+while (str[i] != '\0')
+{
+i++;
 }
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');

32f615bdd5e0fc44993f4a8e76fde5ea3d3fd074
Nmcleon <nmcleon10@gmail.com>
2023-04-03 13:58:01 -0700

    a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: the string to search
- * @accept: the bytes to search for
- *
- *  Return: string s that matches any character specified in accept
- */
+* _strpbrk - searches a string for any of a set of bytes
+* @s: the string to search
+* @accept: the bytes to search for
+*
+*  Return: string s that matches any character specified in accept
+*/
 char *_strpbrk(char *s, char *accept)
 {
 	while (*s)
 	{
 		char *a = accept;
+
 		while (*a)
 		{
 			if (*s == *a)
 			{
-				 return (s);
+				return (s);
 			}
 			a++;
 		}

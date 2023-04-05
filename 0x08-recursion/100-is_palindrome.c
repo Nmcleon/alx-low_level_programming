330b026a60b25fb76f59330b25556f30a7635983
Nmcleon <nmcleon10@gmail.com>
2023-04-05 11:39:41 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,41 +6,47 @@
 *
 * Return: The length of the string
 */
+
 int check(char *s, int start, int end, int pair)
 {
 	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+	{
 		return (1);
+	}
 	else if (s[start] != s[end])
+	{
 		return (0);
+	}
 	else
+	{
 		return (check(s, start + 1, end - 1, pair));
+	}
 }
-
 /**
- * last_index - Returns the last index of a string (counts the null char)
- *
- * @s: Pointer the string
- *
- * Return: int
- */
+* last_index - Returns the last index of a string (counts the null char)
+*
+* @s:
+*
+* Return:
+*/
 
 int last_index(char *s)
 {
 	int n = 0;
 
 	if (*s > '\0')
+	{
 		n += last_index(s + 1) + 1;
-
+	}
 	return (n);
 }
-
 /**
- * is_palindrome - Check if a string is a palindrome
- *
- * @s: String to check
- *
- * Return: 0 or 1
- */
+* is_palindrome - Check if a string is a palindrome
+*
+* @s:
+*
+* Return:
+*/
 
 int is_palindrome(char *s)
 {

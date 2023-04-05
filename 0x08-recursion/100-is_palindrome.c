7af024ef6dc9874be3c8ebd7b34e4e83daba9b98
Nmcleon <nmcleon10@gmail.com>
2023-04-05 11:20:22 -0700

    compiled

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Returns the length of a string
+* @s: The string to get the length of
+*
+* Return: The length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+* is_palindrome - Checks if a string is a palindrome
+* @s:
+*
+* Return:
+*/
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + len - 1))
+	{
+		return (is_palindrome(s + 1) && 1);
+	}
+	return (0);
+}

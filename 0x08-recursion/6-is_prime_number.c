46577ebc671311888ef2865a9e8c41ac4da6854c
N <51336368+Nmcleon@users.noreply.github.com>
2023-04-05 20:29:16 +0200

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,20 +5,27 @@
 *
 * Return: 1 if n is prime, 0 otherwise
 */
+int prime_check(int f, int p)
+{
+	if (p < 2 || p % f == 0)
+		return (0);
+	else if (f > p / 2)
+		return (1);
+	else
+		return (prime_check(f + 1, p));
+}
+
+/**
+ * is_prime_number - States if number is prime
+ *
+ * @n: Number to check
+ *
+ * Return: 1 if prime, 0 if not
+ */
+
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
 }

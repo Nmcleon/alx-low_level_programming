6e99c92d2a70f62fc9b463a568117c0fff8d8950
Nmcleon <nmcleon10@gmail.com>
2023-04-05 11:13:51 -0700

    ./6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,24 @@
 #include "main.h"
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number to check
- *
- * Return: 1 if n is prime, 0 otherwise
- */
+* is_prime_number - checks if a number is prime
+* @n: the number to check
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
 int is_prime_number(int n)
 {
-    int i;
+	int i;
 
-    /* base case: 0, 1, and negative numbers are not prime */
-    if (n <= 1)
-        return (0);
-
-    /* check if n is divisible by any number between 2 and sqrt(n) */
-    for (i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return (0);
-    }
-
-    /* if we haven't returned yet, then n is prime */
-    return (1);
+	if (n <= 1)
+	{
+		return (0);
+	}
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }

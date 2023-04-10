53acc2475ddc0f2067be360479bfd47d3c80d1bd
N <51336368+Nmcleon@users.noreply.github.com>
2023-04-10 19:00:49 +0200

    minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,43 +11,42 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-		}
-		else if (cents > 10)
-		{
-			cents -= 10;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-		}
-		else
-		{
-			cents -= 1;
-		}
-		coins++;
-	}
-	printf("%d\n", coins);
-	return (0);
+int position, total, change, x;
+int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+
+position = total = change = x = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+total = atoi(argv[1]);
+
+if (total <= 0)
+{
+printf("0\n");
+return (0);
+}
+
+/*Declaring While*/
+
+while (coins[position] != '\0')
+
+{
+if (total >= coins[position])
+{
+aux = (total / coins[position]);
+change += x;
+total -= coins[position] * x;
+}
+
+position++;
+
+}
+
+printf("%d\n", change);
+return (0);
 }

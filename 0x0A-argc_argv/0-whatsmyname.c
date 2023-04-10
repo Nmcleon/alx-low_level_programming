20886871233bc40820d511f82c356bfbb84d1ba4
Nmcleon <nmcleon10@gmail.com>
2023-04-10 07:59:52 -0700

    its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print its name
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* return: 0 (Success)
+*/
+
+int main(int argc __attribute__((unused)),
+		char *argv[] __attribute__((unused)))
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

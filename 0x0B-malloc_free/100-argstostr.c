a82e155201d7d583cf91d1eea9d702a235f2f89d
Nmcleon <nmcleon10@gmail.com>
2023-04-11 16:33:00 -0700

    corrections

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,28 +11,62 @@
 * Return: a pointer to thr new string, or NULL
 */
 char *argstostr(int ac, char **av)
-{
-	int i, j, l;
-	char *str;
 
-	if (ac == 0 || av == NULL)
-	{
-		return (NULL);
-	}
-	l = 0;
-	for (i = 0; i < ac; i++)
-		l += strlen(av[i]) + 1;
-	str = malloc(sizeof(char) * l);
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0, j = 0; i < ac; i++)
-	{
-		strcpy(str + j, av[i]);
-		j += strlen(av[i]);
-		str[j++] = '\n';
-	}
-	str[j] = '\0';
-	return (str);
-}
+{
+
+		int i, n, r = 0, l = 0;
+
+			char *str;
+
+
+				if (ac == 0 || av == NULL)
+
+							return (NULL);
+
+
+					for (i = 0; i < ac; i++)
+
+							{
+
+										for (n = 0; av[i][n]; n++)
+
+														l++;
+
+											}
+
+						l += ac;
+
+
+							str = malloc(sizeof(char) * l + 1);
+
+								if (str == NULL)
+
+											return (NULL);
+
+									for (i = 0; i < ac; i++)
+
+											{
+
+													for (n = 0; av[i][n]; n++)
+
+															{
+
+																		str[r] = av[i][n];
+
+																				r++;
+
+																					}
+
+														if (str[r] == '\0')
+
+																{
+
+																			str[r++] = '\n';
+
+																				}
+
+															}
+
+										return (str);
+
+

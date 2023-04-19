9d68fdd926418282ef116e41f379f32fd75255fc
Nmcleon <nmcleon10@gmail.com>
2023-04-19 11:38:11 -0700

    careful

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,22 +3,21 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple operations
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 if success, 98 if incorrect number of arguments,
- *         99 if operator is invalid, or 100 if dividing by 0
- */
+* main - performs simple operations
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0 if success, 98 if incorrect number of arguments,
+*         99 if operator is invalid, or 100 if dividing by 0
+*/
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
-
-    num1 = atoi(argv[1]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);

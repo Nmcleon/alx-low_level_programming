3ffc68fe9baeacd88e5aa1a268b53599e9fd2c31
Nmcleon <nmcleon10@gmail.com>
2023-04-28 07:37:14 -0700

    new node at the end of a list_t

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,28 +4,26 @@
 #include "lists.h"
 
 /**
- * _strlen - Returns the lenght of a string.
- * @s: Type char pointer
- * Return: 0 (SUccess)
- */
+* _strlen - Returns the lenght of a string.
+* @s: Type char pointer
+* Return: 0 (SUccess)
+*/
 int _strlen(const char *s)
 {
 	unsigned int l;
 
 	for (l = 0; s[l] != '\0'; l++)
 	{
-    continue;
+		continue;
 	}
 	return (l);
-
 }
-
 /**
- * add_node_end - Returns the lenght of a string.
- * @str: Type char pointer
- * @head: type pointer of struct
- * Return: new_node.
- */
+* add_node_end - Returns the lenght of a string.
+* @str: beginning of the linked list
+* @head: string to duplicate into the new node
+* Return: address of the new element, or NULL (failed)
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n, *tmp_n_d;
@@ -33,12 +31,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	n = malloc(sizeof(list_t));
 
 	if (n == NULL)
+	{
 		return (NULL);
-
+	}
 	n->str = strdup(str);
 	n->len = _strlen(str);
 	n->next = NULL;
-
 	if (!*head)
 	{
 		*head = n;
@@ -47,7 +45,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		tmp_n_d = *head;
 		while (tmp_n_d->next)
-			tmp_n_d = tmp_n_d->next;
+		tmp_n_d = tmp_n_d->next;
 		{
 			tmp_n_d->next = n;
 		}

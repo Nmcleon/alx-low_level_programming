91b29b0f886683eb8c39ddc72157c2568d3543e6
Nmcleon <nmcleon10@gmail.com>
2023-04-28 07:31:50 -0700

    a new node at the beginning of a list_t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,28 +3,26 @@
 #include <stdlib.h>
 
 /**
- * _strlen - Returns the lenght of a string.
- * @s: Type char pointer
- * Return: 0 (Success)
- */
+* _strlen - Returns the lenght of a string.
+* @s: Type char pointer
+* Return: 0 (Success)
+*/
 int _strlen(const char *s)
 {
 	unsigned int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-    continue; 
+		continue;
 	}
 	return (i);
-
 }
-
 /**
- * *add_node - Return the number of elements in a linked list
- * @head: data type poointer of struct node
- * @str: data type pointer of struct
-* Return: elements of the str i
- */
+* *add_node - Return the number of elements in a linked list
+* @head: beginning of the linked list
+* @str: string to duplicate into the new node
+* Return: new_nide
+*/
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -32,12 +30,12 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
-
+	}
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }

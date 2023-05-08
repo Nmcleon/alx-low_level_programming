db6be3e5ff7642c3ce371a06dda7ceb699bf54e0
N <51336368+Nmcleon@users.noreply.github.com>
2023-05-09 00:10:35 +0200

    we move

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+    ssize_t file, file_read, file_write;
+    char *total_S;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
+    total_S = malloc(sizeof(char) * letters);
+    if (total_S == NULL)
+    {
         return (0);
+    }
     if (filename == NULL)
+    {
         return (0);
+    }
 
     file = open(filename, O_RDONLY);
     if (file == -1)
+    {
         return (0);
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
+    }
+    file_read = read(file, total_S, letters);
+    if (file_read == -1)
+    {
         return (0);
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
+    }
+    file_write = write(STDOUT_FILENO, total_S, file_read);
+    if (file_write == -1)
+    {
         return (0);
+    }
     close(file);
-    free(totalSize);
-    return (fwrite);
+    free(total_S);
+    return (file_write);
 }

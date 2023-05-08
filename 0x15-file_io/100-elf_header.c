e3e57048c820b2e3c4685b17103f7d8018c9501b
N <51336368+Nmcleon@users.noreply.github.com>
2023-05-09 00:18:05 +0200

    test

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <elf.h>
 
-/**
- * get_type - determine file type
- * @ehdr: pointer to struct of ELF header features
- */
-void get_type(Elf64_Ehdr *ehdr)
+#define ERR_MSG "Error: Not an ELF file or an error occurred\n"
+#define ELF_CLASS_STR(class) ((class) == ELFCLASS32 ? "ELF32" : "ELF64")
+#define ELF_DATA_STR(data) ((data) == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian")
+#define ELF_TYPE_STR(type) \
+    ((type) == ET_NONE ? "NONE (No file type)" : \
+     (type) == ET_REL ? "REL (Relocatable file)" : \
+     (type) == ET_EXEC ? "EXEC (Executable file)" : \
+     (type) == ET_DYN ? "DYN (Shared object file)" : \
+     (type) == ET_CORE ? "CORE (Core file)" : \
+     "Unknown")
+
+void print_elf_header(Elf64_Ehdr *hdr)
 {
-	printf("%-35s", "Type:");
-	switch (ehdr->e_type)
-	{
-	case ET_NONE:
-		printf("NONE (Unknown type)\n");
-		break;
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
-	case ET_CORE:
-		printf("CORE (Core file)\n");
-		break;
-	}
+    printf("ELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++)
+        printf("%02x%s", hdr->e_ident[i], i == EI_NIDENT - 1 ? "\n" : " ");
+    printf("  Class:                             %s\n", ELF_CLASS_STR(hdr->e_ident[EI_CLASS]));
+    printf("  Data:                              %s\n", ELF_DATA_STR(hdr->e_ident[EI_DATA]));
+    printf("  Version:                           %d (current)\n", hdr->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", hdr->e_ident[EI_OSABI] == 0 ? "UNIX - System V" : "Other");
+    printf("  ABI Version:                       %d\n", hdr->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", ELF_TYPE_STR(hdr->e_type));
+    printf("  Entry point address:               0x%lx\n", hdr->e_entry);
 }
 
-
-
-/**
- * main - entry point for program to get header of ELF file
- * @argc: arg of count
- * @argv: arg of array
- *
- * Return: 1 on success, error code on failure
- */
 int main(int argc, char *argv[])
 {
-	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
-	return (1);
+    int fd;
+    Elf64_Ehdr hdr;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    if ((fd = open(argv[1], O_RDONLY)) == -1) {
+        perror("open");
+        return 98;
+    }
+
+    if (read(fd, &hdr, sizeof(hdr)) != sizeof(hdr)) {
+        perror("read");
+        return 98;
+    }
+
+    if (hdr.e_ident[EI_MAG0] != ELFMAG0 || hdr.e_ident[EI_MAG1] != ELFMAG1 ||
+            hdr.e_ident[EI_MAG2] != ELFMAG2 || hdr.e_ident[EI_MAG3] != ELFMAG3) {
+        fprintf(stderr, ERR_MSG);
+        return 98;
+    }
+
+    print_elf_header(&hdr);
+    close(fd);
+
+    return 0;
 }

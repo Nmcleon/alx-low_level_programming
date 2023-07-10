fafd7bd7ec7c662a3d09eed2f4554f79eb8c8c33
N <51336368+Nmcleon@users.noreply.github.com>
2023-07-10 09:38:00 +0200

    add executabke and correct 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - copy content of a file to another file
+ * @argc: number of arguments
+ * @argv: argument passed, double pointer
+ * Return: number of letters it could read and print
+ */
+int main(int argc, char **argv)
+{
+    int i, j, k;
+    char buf[1024];
+
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    i = open(argv[1], O_RDONLY);
+    if (i == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    j = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+    while ((k = read(i, buf, 1024)) > 0)
+    {
+        if (write(j, buf, k) != k || j == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+    }
+    if (k == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    if (close(i) < 0)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+        exit(100);
+    }
+    if (close(j) < 0)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", j);
+        exit(100);
+    }
+    return (0);
+}

d543d219b625ba57a11b9ac01d977ac2892e3bab
N <51336368+Nmcleon@users.noreply.github.com>
2023-07-10 09:36:18 +0200

    correct 09 error

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* append_text_to_file - appends text at the end of file
+* @filename: char, the name of the file
+* @text_content: char, the NULL terminated string added at end of file
+* Return: 1 (success), -1 (fail)
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int f_output;
+	int i;
+
+	file = open(filename, O_RDWR | O_APPEND);
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	while (*(text_content + i) != '\0')
+	{
+		i++;
+	}
+	f_output = write(file, text_content, i);
+	if (f_output == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}

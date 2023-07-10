f281baf6877aa1ff2f41f46ad5783c7ee1513dca
Nmcleon <nmcleon10@gmail.com>
2023-07-10 10:23:11 -0700

    betty error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,37 +2,38 @@
 #include "main.h"
 
 /**
-  * append_text_to_file - the function name
-  * @filename: parameter of type const char *.
-  * @text_content: parameter of type char *.
-  * Return: int .
- */
+* append_text_to_file - the function name
+* @filename: parameter of type const char *.
+* @text_content: parameter of type char *.
+* Return: int .
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-int file;
-int f_output;
-int i;
-file = open(filename, O_RDWR | O_APPEND);
-if (filename == NULL)
+	int file;
+	int f_output;
+	int i;
+
+	file = open(filename, O_RDWR | O_APPEND);
+	if (filename == NULL)
 	{
-return (-1);
+		return (-1);
 	}
-if (file == -1)
+	if (file == -1)
 	{
-return (-1);
+		return (-1);
 	}
-if (text_content == NULL)
+	if (text_content == NULL)
 	{
-return (1);
+		return (1);
 	}
-while (*(text_content + i) != '\0')
+	while (*(text_content + i) != '\0')
 	{
-i++;
+		i++;
 	}
-f_output = write(file, text_content, i);
-if (f_output == -1)
+	f_output = write(file, text_content, i);
+	if (f_output == -1)
 	{
-return (-1);
+		return (-1);
 	}
-return (1);
+	return (1);
 }

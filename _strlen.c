e0e813cc6ac119ad39a9bcf6497800e6d37de04c
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-21 20:56:18 +0200

    Create _strlen.c

---
+++ _strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    /* Implementation of the function */
+}

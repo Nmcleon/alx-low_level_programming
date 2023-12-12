39bf8d5fddaaf37e10dbad96d5799c2d371a6e51
Nmcleon <nmcleon10@gmail.com>
2023-12-12 00:35:20 -0800

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,27 @@
+#include "search_algos.h"
 
+/**
+* linear_search - Searches for a value in an array of int
+* @array: Pointer to the first element of the array to search in
+* @size: Number of elements in the array
+* @value: Value to search for
+* Return: first index (SUCCESS) or -1 (FAIL)
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}

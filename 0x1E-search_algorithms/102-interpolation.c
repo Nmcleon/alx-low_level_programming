4169ee72e96f19ba3b5c96b6954da7d1de8b39bd
Nmcleon <nmcleon10@gmail.com>
2023-12-12 01:46:03 -0800

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+* interpolation_search - the function name
+* @array: parameter of type int *.
+* @size: parameter of type size_t .
+* @value: parameter of type int .
+* Return: int .
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	sze_t a = 0, b = size - 1, m = 0;
+
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+	while (array[a] != array[b])
+	{
+		m = a + (value - array[a]) * (b - a) / (array[b] - array[a]);
+		if (m >= size)
+		{
+			printf("Value checked array[%lu] is out of range\n", m);
+			return (-1);
+		}
+		printf("Value checked array[%lu] = [%d]\n", m, array[m]);
+		if (array[m] < value)
+		{
+			a = m + 1;
+		}
+		else if (array[m] > value)
+		{
+			b = m - 1;
+		}
+		else
+		{
+			return (m);
+		}
+	}
+	return (-1);
+}
